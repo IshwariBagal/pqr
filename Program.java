3eeeae84dc8514ace44bf632049178ea3366e992
IshwariBagal <ishwari.bagal_24uds@sanjivani.edu.in>
2024-12-30 16:57:23 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -2,6 +2,6 @@ public class Program
   {
     public static void main(String args[])
     {
-      System.out.println("Welcome to CI/CD pipeline");
+      System.out.println("Welcome to CI/CD pipeline !");
     }
   }
